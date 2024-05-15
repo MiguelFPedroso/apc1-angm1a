bd710ce2c66b134eaff9d41adefc89173ab546ee
MiguelFPedroso <131493206+MiguelFPedroso@users.noreply.github.com>
2024-05-15 13:53:35 +0000

    aula 7 e pratica 3

---
+++ aulas/aula07/validacao.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+  int numero;
+  int numero_valido = 0;
+
+  while (numero_valido == 0) {
+    printf("Entre com um número de 1 a 10: ");
+    int deu_certo = scanf("%i", &numero);
+    int numero_valido = numero >= 1 && numero <= 10;
+
+    if (deu_certo && numero_valido) {
+      printf("Segue o jogo!\n");
+      break;
+    } else {
+      getchar(); // limpar \n
+      printf("Numero invalido. Tente de novo!\n");
+    }
+  }
+
+   do {
+    printf("Entre com um número de 1 a 10: ");
+    int deu_certo = scanf("%i", &numero);
+    int numero_valido = numero >= 1 && numero <= 10;
+
+    if (deu_certo && numero_valido) {
+      printf("Segue o jogo!\n");
+      break;
+    } else {
+       getchar(); // limpar \n
+      printf("Numero invalido. Tente de novo!\n");
+    }
+  }  while (numero_valido == 0);
+  return 0;
+}
